af709d21c09d6e02e910ec405cf274c3df8fb067
Yunus İNAN <yns.inan1660@gmail.com>
2022-01-24 19:34:20 +0300

    Routes Pages Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Navbar } from "components";
-import Home from "pages/Home/Home";
-import { useState } from "react";
+import { lazy, Suspense, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme, GlobalStyles } from "Themes/theme";
+
+const Home = lazy(() => import("./pages/Home/Home"));
+const Favorites = lazy(() => import("./pages/Favorites/Favorites"));
+const Basket = lazy(() => import("pages/Basket/Basket"));
 
 function App() {
   const [theme, setTheme] = useState("light");
@@ -11,12 +14,16 @@ function App() {
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
       <GlobalStyles />
 
-      <Navbar theme={theme} setTheme={setTheme} />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <BrowserRouter>
+          <Navbar theme={theme} setTheme={setTheme} />
+          <Routes>
+            <Route path="/" element={<Home theme={theme}/>} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/basket" element={<Basket />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
     </ThemeProvider>
   );
 }
