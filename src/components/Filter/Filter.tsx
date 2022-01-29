5149ec9bad99f8d3f0b186ec4a4cd0fe291ba8a8
Yunus İNAN <yns.inan1660@gmail.com>
2022-01-29 18:55:27 +0300

    Filter Component New Feature İmprovements

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 
-import { filterByName } from "store/actions/ProductActions";
+import { filterByName, sortPriceProduct } from "store/actions/ProductActions";
 import * as S from "./styles";
 
 const Filter = () => {
   const [inputValue, setInputValue] = useState("");
+  const [sortPrice, setSortPrice] = useState("less to more");
   const dispatch = useDispatch();
 
   const handleSearch = (
@@ -15,6 +16,14 @@ const Filter = () => {
     dispatch(filterByName(e.currentTarget.value));
   };
 
+  const handleRadio = (e: any) => {
+    setSortPrice(e.target.value);
+    dispatch(sortPriceProduct(e.target.value));
+  };
+
+  useEffect(() => {
+    console.log(sortPrice);
+  }, [sortPrice]);
   return (
     <div>
       <S.Input
@@ -24,6 +33,26 @@ const Filter = () => {
         value={inputValue}
         onChange={handleSearch}
       />
+      <div className="filterElement">
+        <input
+          type="radio"
+          name="less to more"
+          value="less to more"
+          checked={sortPrice === "less to more"}
+          onChange={handleRadio}
+        />
+        <label htmlFor="less to more">Less to more</label>
+      </div>
+      <div className="filterElement">
+        <input
+          type="radio"
+          name="more to less"
+          value="more to less"
+          onChange={handleRadio}
+          checked={sortPrice === "more to less"}
+        />
+        <label htmlFor="more to less">More to less</label>
+      </div>
     </div>
   );
 };
